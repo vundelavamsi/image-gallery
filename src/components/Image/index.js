6c240b70f2c256fb41bef165b8baba76c2927c7a
Vundela Vamsi <vr313009@gmail.com>
2024-05-17 16:17:18 +0530

    add popup on hover effect

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import './index.css'
+import React from "react";
+import "./index.css";
 
-const Image = ({eachImage}) => {
+const Image = ({ eachImage }) => {
   return (
-    <li className='each-image'>
-        <img src={eachImage.urls.small} className='image' />
+    <li className="each-image">
+      <img src={eachImage.urls.small} className="image" />
+      <div className="image-description">
+        <span>{eachImage.alt_description}</span>
+      </div>
     </li>
-  )
-}
+  );
+};
 
-export default Image
+export default Image;
